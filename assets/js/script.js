be07731f7ce0c7cde1bb9827d97674bc467bbc62
kaylab78 <kaylabackus78@gmail.com>
2022-06-09 12:26:17 -0500

    Added pseudocode to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// When the user clicks on a series from the dropdown menu, they are presented with two random characters from that series.
+
+// When the user clicks the Remix button, they are presented with two new random characters from the same series.
+
+// When the user clicks the Accept button, a random quote appears on the screen.
+
+// When the user clicks the Save button, the characters and quote are saved to a card at the bottom of the screen.
+
+// When the user refreshes the page, the previously saved cards stay on the page.
+
+// When the user clicks the Clear button, the previous results are removed from the page.
