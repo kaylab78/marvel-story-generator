2bd9b7463e2bfb4ad1f4c44066e8e82aebad9e81
kaylab78 <kaylabackus78@gmail.com>
2022-06-11 10:13:22 -0500

    Characters returned to console

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MARVELous Writing Prompts
+
+## Credits
+*Function md5().* Function md5(). [http://www.md5.cz/](http://www.md5.cz/).
+
+## License
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ var characterListEl = document.getElementById("character-list");
 var thorEl = document.getElementById("thor");
 
 // When the user clicks on a series from the dropdown menu, they are presented with two random characters from that series.
-function getCharacters(event) {
-    var characterSelection = event.target.value
+function getCharacters() {
+    // var characterSelection = event.target.value
 
+    // Reference: Function md5() used to generate the hash needed to call the API.
     var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts=2020&apikey=33006268417691bd580b5aafb863584a&hash=35194bc0e16921b8664b670b6ea93832";
     
 
@@ -13,7 +14,7 @@ function getCharacters(event) {
         .then(data => console.log(data));
 }
 
-characterListEl.addEventListener("change", getCharacters);
+characterListEl.addEventListener("click", getCharacters);
 
 // When the user clicks the Remix button, they are presented with two new random characters from the same series.
 
