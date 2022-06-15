e845c0bfdb4e14e15b1fcf14de639ef0b74031aa
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-06-14 20:25:47 -0500

    added clear function and hidden classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,3 @@
-var generatedQuote = document.querySelector("#generatedQuote");
-var authorQuote = document.querySelector("#authorQuote")
-var quoteSave = document.querySelector("#quoteSv");
-var accept = document.querySelector("#acceptBtn");
-var clearBtn = document.querySelector("#clear")
-accept.addEventListener("click", function(){
-    quoteApi();
-})
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4f9f7df054msh309ee5f762d1c9ep14b973jsnebf9ab04b1dd',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
-var quoteApi = function() {
-    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-	.then(response => response.json())
-	// .then(response => console.log(response))
-    .then(response => {
-        // remove class hidden from save/clear
-        quoteSave.classList.remove("hidden")
-        clearBtn.classList.remove("hidden")
-        var quote = response.content;
-        var quoteAuthor = response.originator.name;
-        generatedQuote.innerHTML = "'" +quote + "'"
-        authorQuote.innerHTML = "'" +quoteAuthor+"'"
-    })
-	.catch(err => console.error(err));
-}
 var characterListEl = document.getElementById("character-list");
 var errorMsgEl = document.getElementById("error-message");
 var characterOneEl = document.getElementById("character-one");
@@ -35,6 +5,15 @@ var characterTwoEl = document.getElementById("character-two");
 var firstImageEl = document.getElementById("first-image");
 var secondImageEl = document.getElementById("second-image");
 var choiceBtnsEl = document.getElementById("choice-btns")
+var generatedQuote = document.querySelector("#generatedQuote");
+var authorQuote = document.querySelector("#authorQuote")
+var quoteSave = document.querySelector("#quoteSv");
+var accept = document.querySelector("#acceptBtn");
+var clearBtn = document.querySelector("#clear")
+var remix = document.querySelector("#remix")
+
+var quote;
+var quoteAuthor;
 
 // When the user clicks on a series from the dropdown menu, they are presented with two random characters from that series.
 function getCharacters() {
@@ -82,4 +61,62 @@ function randomizeCharacters (data) {
     
 }
 
-characterListEl.addEventListener("click", getCharacters);
+
+// 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4f9f7df054msh309ee5f762d1c9ep14b973jsnebf9ab04b1dd',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+var quoteApi = function() {
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	// .then(response => console.log(response))
+    .then(response => {
+        // remove class hidden from save/clear
+        quoteSave.classList.remove("hidden")
+        clearBtn.classList.remove("hidden")
+        quote = response.content;
+        quoteAuthor = response.originator.name;
+        generatedQuote.innerHTML = "'" +quote + "'"
+        authorQuote.innerHTML = "'" +quoteAuthor+"'"
+    })
+	.catch(err => console.error(err));
+}
+// reset function to clear content on page to start over
+var reset =function () {
+    generatedQuote.innerHTML ="";
+    authorQuote.innerHTML="";
+    quoteSave.classList.add("hidden");
+    clearBtn.classList.add("hidden");
+    choiceBtnsEl.classList.add("hidden");
+    characterOneEl.textContent = "";
+    characterTwoEl.textContent = "";
+    firstImageEl.removeAttribute("src")
+    secondImageEl.removeAttribute("src")
+}
+// gets two characters at random
+characterListEl.addEventListener("click", function(){
+    getCharacters();
+    // allows to change characters before accepting
+    remix.innerHTML = "Remix Characters"
+    accept.classList.remove("hidden")
+})
+
+// accept button accepts the characters from characterListEl and generates a quote
+accept.addEventListener("click", function(){
+    quoteApi();
+    characterListEl.classList.add("hidden")
+    accept.classList.add("hidden")
+})
+// clear button resets the content
+clearBtn.addEventListener("click" , function(){
+    reset();
+    characterListEl.classList.remove("hidden")
+    remix.innerHTML = "Get Characters"
+    
+    
+    
+})
