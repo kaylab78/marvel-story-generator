ec03b2583a011262499c5fc41dfb0905ea316612
kaylab78 <kaylabackus78@gmail.com>
2022-06-15 16:46:39 -0500

    Wrote README and cleaned up js and html

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-# MARVELous Writing Prompts
+# MARVELous Writing Prompt
+
+## Description
+The MARVELous Writing Prompt is a writing prompt generator for creative writers with an interest in the Marvel comic universe. It's purpose is to give the user inspration for their writing prompt.
+
+Using the Marvel API, we are able to present to the user two random characters and one random quote through the Quotes API from RapidAPI for them to use in their writing. 
+
+This generator can be used by creative writing classes, writing clubs, fanfiction writers, etc.
+
+## Installation
+In order to access this repository, clone it from GitHub using the command <samp>git clone git@github.com:kaylab78/marvel-story-generator.git</samp> in the command line interface.
+
+## Usage
+To view the deployed website, [click here](https://kaylab78.github.io/marvel-story-generator).
+
+When the user opens the webpage, they are presented with a button that says, "Get Characters." When the user clicks this button, two characters from the Marvel universe appear on the page.
+![The screen shows instructions for the user and has a black button with white letters that say, "Get Characters.](/assets/images/screenshot-1.png)
+
+If the user doesn't like their character choices, then they can click the "Remix Characters" button to get two new characters.
+
+If the user likes these choices for their writing prompt, then they click the "Accept" button to generate a random quote.
+![The screen now shows Nighthawk and Nekra from the Marvel Universe. The "Get Characters" button now says, "Remix Characters," and a smaller black button appeared below the characters that says, "Accept."](/assets/images/screenshot-2.png)
+
+To save their combination of the characters and quote, the user clicks the "Save" button. To clear their entire combination, the user clicks the "Clear" button.
+![The screen now says, "The difference between where you are now and where you want to be is your reasoning capacity and actions. Hamis Kiggundu," who is the author of the quote.Two black buttons appeared at the bottom of the screen that say, "Save" and "Clear."](/assets/images/screenshot-3.png)
+
+Once the combination is saved, the page is cleared for a new combination. All previous combinations appear at the bottom of the screen.
+![The screen has been cleared of characters and quotes. Now in the bottom left corner, it says, "Nighthawk and Nekra. The difference between where you are now and where you want to be is your reasoning capacity and actions. Hamis Kiggundu."](/assets/images/screenshot-4.png)
+
+## Technologies
+[Foundation for Sites](https://get.foundation/sites/docs/)
+
+[jQuery](https://jquery.com/)
+
+[Marvel API](https://developer.marvel.com/)
+
+[MIT License](https://choosealicense.com/licenses/mit/)
+
+[Quotes API](https://rapidapi.com/martin.svoboda/api/quotes15/)
+
+## Future Development
+One thing to address in a future version of this webpage is that some of the Marvel characters don't have photos associated with them. When that's the case, the API returns an image that says, "Image not available."
+![The screen shows two images of silhouettes, and the images say, "Image not available."](/assets/images/screenshot-5.png)
+
+A possible way to fix this would be to include an if statement that when the thumbnail path is equal to "image_not_available," the script chooses a new index number until it comes to a character that has an image associated with it.
+
+Another thing to address would be branding the webpage to match the aesthetic that Marvel has created for themselves.
+
+Plus, it would be nice if the user could choose to get random characters from a specific series. This would need to use a different variation on the API call. 
+
+In addition to these, we would also like to address the fact that the API will only call 100 consecutive results at a time, so the character choices aren't truly random. And if data for all 100 characters is called, there is a significant delay in loading on the page. This will lead to a negative user experience.
 
 ## Credits
 *Function md5().* Function md5(). [http://www.md5.cz/](http://www.md5.cz/).
@@ -8,3 +58,10 @@
 *Interactive API.* Marvel. [https://developer.marvel.com/docs#!/public/](https://developer.marvel.com/docs#!/public/).
 
 ## License
+Copyright (c) 2022 Kayla Backus, Mitchel Eide, Angela Gustafson, Tim Leonard, Ryan Rogers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,18 @@ var characterTwoEl = document.getElementById("character-two");
 var firstImageEl = document.getElementById("first-image");
 var secondImageEl = document.getElementById("second-image");
 var choiceBtnsEl= document.getElementById("choice-btns");
-var saveBtnEl = document.getElementById("quoteSv")
+var saveBtnEl = document.getElementById("quoteSv");
 var characterOne;
 var characterTwo;
 var generatedQuote = document.querySelector("#generatedQuote");
-var authorQuote = document.querySelector("#authorQuote")
+var authorQuote = document.querySelector("#authorQuote");
 var quoteSave = document.querySelector("#quoteSv");
 var accept = document.querySelector("#acceptBtn");
-var clearBtn = document.querySelector("#clear")
-var remix = document.querySelector("#remix")
+var clearBtn = document.querySelector("#clear");
+var remix = document.querySelector("#remix");
 var quote;
 var quoteAuthor;
-var savedResults 
+var savedResults;
 var charNames = document.querySelector("#char-names");
 var persistentData = document.querySelector("#persistent-data");
 var saveResultsData = {
@@ -24,18 +24,19 @@ var saveResultsData = {
     charTwo: characterTwo,
     chosenQuote: quote,
     chosenAuthor: quoteAuthor
-}
+};
 
-// When the user clicks on a series from the dropdown menu, they are presented with two random characters from that series.
+// When the user clicks on the Get Characters button, they are presented with two random characters from that series.
 function getCharacters() {
-    choiceBtnsEl.classList.remove("hidden")
+    choiceBtnsEl.classList.remove("hidden");
 
     // The limit parameter in the API limits the number of results to 100 or less. The offset parameter skips that number of results and returns the remaining results. For example, if the limit=100 and the offset=1500, the results will start at 1500 and return 62 because there are a total of 1562 characters avaialble to choose from. In order to give the user the most character choices, we've implemented a Math.random to the offset parameter.  
     var characterStart = Math.floor(Math.random() * 1562);
 
     // Reference: Function md5() used to generate the hash needed to call the API.
-    var apiUrl = "https://gateway.marvel.com/v1/public/characters?&limit=100&offset=" + characterStart + "&ts=2020&apikey=33006268417691bd580b5aafb863584a&hash=35194bc0e16921b8664b670b6ea93832";
+    var apiUrl = "https://gateway.marvel.com/v1/public/characters?&limit=35&offset=" + characterStart + "&ts=2020&apikey=33006268417691bd580b5aafb863584a&hash=35194bc0e16921b8664b670b6ea93832";
 
+    // Call the Marvel API
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
@@ -52,37 +53,45 @@ function getCharacters() {
 
 function randomizeCharacters (data) {
     var characterArray = data.data.results;
-    console.log(data.data.results);
+    // console.log(data.data.results);
 
+    // Random number in the result index
     var firstRandom = Math.floor(Math.random() * characterArray.length);
     var secondRandom = Math.floor(Math.random() * characterArray.length);
 
+    // Select two random characters from the result array
     characterOne = characterArray[firstRandom].name;
     characterTwo = characterArray[secondRandom].name;
+
     saveResultsData.charOne = characterOne;
     saveResultsData.charTwo = characterTwo;
-
-    var firstImage = characterArray[firstRandom].thumbnail.path + "/landscape_large.jpg";
-    var secondImage = characterArray[secondRandom].thumbnail.path + "/landscape_large.jpg";
 
     characterOneEl.textContent = characterOne;
     characterTwoEl.textContent = characterTwo;
 
+    // Corresponding images for the characters
+    var firstImage = characterArray[firstRandom].thumbnail.path + "/landscape_large.jpg";
+    var secondImage = characterArray[secondRandom].thumbnail.path + "/landscape_large.jpg";
+
     firstImageEl.setAttribute("src", firstImage);
     secondImageEl.setAttribute("src", secondImage);
+};
 
-    // Call function to get quote.
-}
+// gets two characters at random
+characterListEl.addEventListener("click", function(){
+    getCharacters();
+    // allows to change characters before accepting
+    remix.innerHTML = "Remix Characters";
+    accept.classList.remove("hidden")
+});
 
-characterListEl.addEventListener("click", getCharacters);
-
-// When the user clicks the Remix button, they are presented with two new random characters from the same series.
-
-// When the user clicks the Accept button, a random quote appears on the screen.
-
+// accept button accepts the characters from characterListEl and generates a quote
 accept.addEventListener("click", function(){
     quoteApi();
-})
+    characterListEl.classList.add("hidden");
+    accept.classList.add("hidden");
+});
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -90,10 +99,10 @@ const options = {
 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
 	}
 };
+
 var quoteApi = function() {
     fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
 	.then(response => response.json())
-	// .then(response => console.log(response))
     .then(response => {
         quoteSave.classList.remove("hidden")
         clearBtn.classList.remove("hidden")
@@ -105,10 +114,7 @@ var quoteApi = function() {
         saveResultsData.chosenAuthor = quoteAuthor;
     })
 	.catch(err => console.error(err));
-}
-
-// When the user clicks the Clear button, the previous results are removed from the page.
-        // remove class hidden from save/clear
+};
 
 // When the user clicks the Save button, the characters and quote are saved to a card at the bottom of the screen.
 
@@ -120,29 +126,27 @@ function saveSelection () {
     window.location.reload();
 }
 
+// var saveResults = function () {
+//     localStorage.setItem("saved",JSON.stringify(saveResultsData));
+// }
+
+// Print character and quote selection to the page
 function renderSelection () {
-    console.log(JSON.parse(localStorage.getItem("saved")))
+    // console.log(JSON.parse(localStorage.getItem("saved")));
     for (let i = 0; i < savedResults.length; i++) {
-        var dataCard = document.createElement("div")
-    dataCard.setAttribute("class", "cell small-8 small-offset-1 medium-6 medium-offset-1 large-3 large-offset-1");
-    dataCard.innerHTML = "<p><strong>" + savedResults[i].charOne + ", " + savedResults[i].charTwo + "</strong><br>" + savedResults[i].chosenQuote + "<br>" + savedResults[i].chosenAuthor + "</p>"
-    persistentData.appendChild(dataCard);
+        var dataCard = document.createElement("div");
+        dataCard.setAttribute("class", "cell small-8 small-offset-1 medium-6 medium-offset-1 large-3 large-offset-1");
+        dataCard.innerHTML = "<p><strong>" + savedResults[i].charOne + " & " + savedResults[i].charTwo + "</strong><br>" + savedResults[i].chosenQuote + "<br>" + savedResults[i].chosenAuthor + "</p>";
+        persistentData.appendChild(dataCard);
     };
 }
-
-// savedResults.addEventListener("click", saveSelection);
-
-var saveResults = function () {
-    localStorage.setItem("saved",JSON.stringify(saveResultsData));
-}
-
-saveBtnEl.addEventListener("click", saveSelection);
 
 // When the user refreshes the page, the previously saved cards stay on the page.
 function loadHistory () {
     savedResults = JSON.parse(localStorage.getItem("saved")) || [];
 }
 
+saveBtnEl.addEventListener("click", saveSelection);
 loadHistory ();
 renderSelection ();
 
@@ -155,27 +159,13 @@ var reset =function () {
     choiceBtnsEl.classList.add("hidden");
     characterOneEl.textContent = "";
     characterTwoEl.textContent = "";
-    firstImageEl.removeAttribute("src")
-    secondImageEl.removeAttribute("src")
+    firstImageEl.removeAttribute("src");
+    secondImageEl.removeAttribute("src");
 }
-// gets two characters at random
-characterListEl.addEventListener("click", function(){
-    getCharacters();
-    // allows to change characters before accepting
-    remix.innerHTML = "Remix Characters"
-    accept.classList.remove("hidden")
-})
 
-// accept button accepts the characters from characterListEl and generates a quote
-accept.addEventListener("click", function(){
-    quoteApi();
-    characterListEl.classList.add("hidden")
-    accept.classList.add("hidden")
-})
 // clear button resets the content
 clearBtn.addEventListener("click" , function(){
     reset();
-    characterListEl.classList.remove("hidden")
-    remix.innerHTML = "Get Characters"
+    characterListEl.classList.remove("hidden");
+    remix.innerHTML = "Get Characters";
 })
-
